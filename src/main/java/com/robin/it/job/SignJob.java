a036ffc13f3c88bffb4b0eac7f086c93824ca486
boxfcg-v <pinghu198538570@163.com>
2021-01-07 23:44:56 +0800

    add  cronjob attribute 定时任务功能

---
+++ src/main/java/com/robin/it/job/SignJob.java
@@ -0,0 +1,13 @@
+package com.robin.it.job;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SignJob {
+	
+	
+	public void doJob(){
+		System.out.println("dddd====================");
+	}
+
+}
